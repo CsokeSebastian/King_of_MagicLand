c6249e7d915851fc1f0f2c0d0abeefcaa18899b0
CsokeSebastian <csokesebastian@yahoo.ro>
2022-09-24 22:41:04 +0200

    Modified the inventory.

--- players.py
+++ players.py
@@ -4,25 +4,32 @@ class Player:
         self.name = type_of
         self.life = life
         self.power = power
-        self.inventory = []
         self.attack = attack
 
 
 class Warrior(Player):
     def __init__(self):
         super().__init__("Warrior", 100, 50, 25)
+        self.inventory = {"Wooden axe": 10,
+                          "Wooden shield": 20}
 
 
 class Wizard(Player):
     def __init__(self):
         super().__init__("Wizard", 50, 100, 25)
+        self.inventory = {"Wooden stick": 20,
+                          "Short lasting shield": 10}
 
 
 class Elf(Player):
     def __init__(self):
         super().__init__("Elf", 75, 75, 25)
+        self.inventory = {"Wooden sword": 10,
+                          "Magic shield": 20}
 
 
 class Archer(Player):
     def __init__(self):
         super().__init__("Archer", 40, 150, 30)
+        self.inventory = {"Wooden bow": 20,
+                          "Wooden shield": 10}
