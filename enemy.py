55f6d9a396dc64777a2e6d5e92057e7955612ea0
CsokeSebastian <csokesebastian@yahoo.ro>
2022-10-01 19:08:09 +0200

    Created the random enemy selection

--- enemy.py
+++ enemy.py
@@ -1,3 +1,5 @@
+import random
+
 
 class Enemy:
     def __init__(self, type_of, life, attack):
@@ -21,3 +23,18 @@ class Troll(Enemy):
 
     def __init__(self):
         super().__init__("Troll", 150, 50)
+
+
+
+def enemy_sel():
+
+    goblin = Goblin()
+    orc = Orc()
+    troll = Troll()
+    enemy_opt = (goblin, orc, troll)
+    enemy = random.choice(enemy_opt)
+    print(f"Your enemy is: {enemy.type_of}\nattack: {enemy.attack}\nlife: {enemy.life}")
+    return enemy
+
+
+bad_char = enemy_sel()
